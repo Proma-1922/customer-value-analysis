5a244906153947d38d385371ef873083f1e4a686
Sumaiya Sarrat <101738443+Proma-1922@users.noreply.github.com>
2023-09-24 23:07:35 -0400

    Add

--- main.py
+++ main.py
@@ -51,3 +51,25 @@ fig = px.bar(roi_by_channel,
              x='channel', 
              y='roi', title='Return on Investment (ROI) by Channel')
 fig.show()
+
+data['cltv'] = (data['revenue'] - data['cost']) * data['conversion_rate'] / data['cost']
+
+channel_cltv = data.groupby('channel')['cltv'].mean().reset_index()
+
+fig = px.bar(channel_cltv, x='channel', y='cltv', color='channel',
+             title='Customer Lifetime Value by Channel')
+
+fig.update_xaxes(title='Channel')
+fig.update_yaxes(title='CLTV')
+
+fig.show()
+
+subset = data.loc[data['channel'].isin(['social media', 'referral'])]
+
+fig = px.box(subset, x='channel', y='cltv', title='CLTV Distribution by Channel')
+
+fig.update_xaxes(title='Channel')
+fig.update_yaxes(title='CLTV')
+fig.update_layout(legend_title='Channel')
+
+fig.show()
