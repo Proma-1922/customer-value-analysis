663deb1f0d6915f424dec6d813184bea7091c945
Sumaiya Sarrat <101738443+Proma-1922@users.noreply.github.com>
2023-09-24 23:02:54 -0400

    created main with histogram analysis

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.io as pio
+pio.templates.default = "plotly_white"
+
+data = pd.read_csv("customer_acquisition_data.csv")
+print(data.head())
+
+fig = px.histogram(data, 
+                   x="cost", 
+                   nbins=20, 
+                   title='Distribution of Acquisition Cost')
+fig.show()
+
+fig = px.histogram(data, 
+                   x="revenue", 
+                   nbins=20, 
+                   title='Distribution of Revenue')
+fig.show()
+
+cost_by_channel = data.groupby('channel')['cost'].mean().reset_index()
+
+fig = px.bar(cost_by_channel, 
+             x='channel', 
+             y='cost', 
+             title='Customer Acquisition Cost by Channel')
+fig.show()
